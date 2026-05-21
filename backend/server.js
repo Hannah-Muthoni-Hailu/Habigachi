3c5093b4af0a8293a2dc29155ae544742a992688
Hannah Hailu Muthoni <muthonihannahhailu@gmail.com>
2026-05-21 18:41:40 +0000

    Database setup complete

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const PORT = 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+
+let db;
+
+async function startServer() {
+    try {
+        await client.connect();
+        db = client.db('habigachi');
+        console.log('Connected successfully to MongoDB');
+
+        app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+    } catch (error) {
+        console.error('Database connection failed:', error);
+        process.exit(1);
+    }
+}
+
+app.get('/signup', (req, res) => {
+    
 })
 
-app.listen(PORT, () => {
-    console.log(`Application running on ${PORT}`)
-})
+startServer();
