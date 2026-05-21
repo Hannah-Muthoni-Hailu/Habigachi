5082ffa32045c1eeb83bd4dab98019d2addcb878
Hannah Hailu Muthoni <muthonihannahhailu@gmail.com>
2026-05-21 19:38:52 +0000

    Signup and login backend routes created

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const { MongoClient } = require('mongodb');
+const { body, validationResult } = require('express-validator');
 require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
 app.use(express.json());
+app.use(cors());
 
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
@@ -24,8 +27,53 @@ async function startServer() {
     }
 }
 
-app.get('/signup', (req, res) => {
-    
-})
+app.post('/signup', 
+    body('username').notEmpty().withMessage('Please enter a username').isLength({ min: 5 }).withMessage('Username must have more than 5 characters'),
+    body('email').notEmpty().withMessage('Please enter an email address').isEmail().withMessage('Please enter a valid email'),
+    body('password').notEmpty().withMessage('Please enter a password').isLength({ min: 5 }).withMessage('Password must have more than 5 characters'),
+    async (req, res) => {
+        const result = validationResult(req);
+        if (result.isEmpty()) {
+            try {
+                let existing_users = await db.collection('users').find({ email: req.body.email }).toArray();
+
+                if (existing_users.length === 0) {
+                    const dbResult = await db.collection('users').insertOne({
+                        'username': req.body.username,
+                        'email': req.body.email,
+                        'password': req.body.password
+                    });
+                    res.status(200).json({ message: 'New user created successfully!' });
+                } else {
+                    res.status(500).json({ error: "The email already exists. Please provide a different one or log in" })
+                }
+                
+            } catch (error) {
+                res.status(500).json({ error: error.message });
+            }
+        } else {
+            res.status(400).json({ errors: result.array() })
+        };
+});
+
+app.post('/login',
+    body('email').notEmpty().withMessage('Email cannot be empty').isEmail().withMessage('Please enter a valid email'),
+    body('password').notEmpty().withMessage('Password cannot be empty').isLength({ min: 5 }).withMessage('Please enter a valid password'),
+    async (req, res) => {
+        const results = validationResult(req)
+
+        if (results.isEmpty()) {
+            let existing_users = await db.collection('users').find({ email: req.body.email, password: req.body.password }).toArray();
+
+            if (existing_users.length === 1) {
+                res.status(200).json({ message: 'User successfully logged in!' });
+            } else {
+                res.status(500).json({ error: 'Invalid email or password' });
+            }
+        } else {
+            res.status(500).json({ error: results.array() });
+        }
+    }
+)
 
 startServer();
